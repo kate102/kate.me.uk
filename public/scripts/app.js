eac9c4f68964f891410adc343c326cba6b2c1b73
Kate Morris <kate.morris.new@gmail.com>
2020-01-19 15:07:12 +0000

    Created outline

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,33 @@ console.log('App.js is running');
 // JSX Javascripit synatx extension
 // Provided my React
 var template = React.createElement(
-  'h1',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Kates App'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        'Education'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        'Work Experience'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        'Roles and Responsibilites'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        'Hobbies'
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -2,7 +2,15 @@ console.log('App.js is running');
 
 // JSX Javascripit synatx extension
 // Provided my React
-var template = <h1>Does this change?</h1>;
+var template =  (
+    <div>
+        <h1>Kates App</h1>
+        <h2>Education</h2>
+        <h2>Work Experience</h2>
+        <h2>Roles and Responsibilites</h2>
+        <h2>Hobbies</h2>
+    </div>
+)
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
